19d0f9103e4d347b3be92b322442e7e127ed6cd4
Oleh <s96481@pollub.edu.pl>
2025-12-04 23:05:20 +0100

    Add model Book

---
+++ src/main/java/com/example/libraryproject/model/Book.java
@@ -0,0 +1,62 @@
+package com.example.libraryproject.model;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    @Column(unique = true)
+    private String isbn;
+    private boolean available = true;
+
+    public Book() {}
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = true;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title='" + title + '\'' +
+                ", author='" + author + '\'' + ", isbn='" + isbn + '\'' +
+                ", available=" + available + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
