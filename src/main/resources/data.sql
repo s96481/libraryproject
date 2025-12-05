8c6354c43b241c8b87aeac2d49d79588b611cec3
Oleh <s96481@pollub.edu.pl>
2025-12-05 01:04:29 +0100

    database configuration and data.sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO books (id, title, author, isbn, available) VALUES (1, 'Kod Leonarda da Vinci', 'Dan Brown', '9788300000001', TRUE);
+INSERT INTO books (id, title, author, isbn, available) VALUES (2, 'Harry Potter', 'J.K. Rowling', '9788300000002', TRUE);
+INSERT INTO books (id, title, author, isbn, available) VALUES (3, 'Władca Pierścieni', 'J.R.R. Tolkien', '9788300000003', TRUE);
+INSERT INTO books (id, title, author, isbn, available) VALUES (4, 'Jakp okochałam gangstera', 'Maciej Kawulski', '9788300000004', TRUE);
+INSERT INTO books (id, title, author, isbn, available) VALUES (5, 'Berserk', 'Kentaro Miura', '9788300000005', TRUE);
+INSERT INTO books (id, title, author, isbn, available) VALUES (6, 'Jak zostałem gangsterem', 'Maciej Kawulski', '9788300000006', TRUE);
+INSERT INTO books (id, title, author, isbn, available) VALUES (7, 'Pan Tadeusz', 'Adam Mickiewicz', '9788300000007', TRUE);
+
+INSERT INTO readers (id, name, email) VALUES (1, 'Oleh Andreichuk', 'oandreichuk@example.com');
+INSERT INTO readers (id, name, email) VALUES (2, 'Michał Filipczak', 'mfilipczak@example.com');
+INSERT INTO readers (id, name, email) VALUES (3, 'Anna Kowalska', 'akowalska@example.com');
+INSERT INTO readers (id, name, email) VALUES (4, 'Jan Nowak', 'jnowak@example.com');
+INSERT INTO readers (id, name, email) VALUES (5, 'Alfa Romeo', 'aromeo@example.com');
+INSERT INTO readers (id, name, email) VALUES (6, 'Paweł Wileki', 'pwielki@example.com');
+INSERT INTO readers (id, name, email) VALUES (7, 'Olek Zdrowy', 'ozdrowy@example.com');
